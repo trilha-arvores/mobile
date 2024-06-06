b3db2a9c2b74234508318e92bccc2ce25d9ded3c
Vítor Fróis <vitor0frois@gmail.com>
2024-06-06 14:32:37 -0300

    trilhas style

--- src/screens/TrilhasScreen.js
+++ src/screens/TrilhasScreen.js
@@ -1,18 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList, ActivityIndicator, Text, Pressable } from 'react-native';
+import { View, FlatList, ActivityIndicator, Text, Pressable, Image } from 'react-native';
 import { styles } from '../styles/styles';
 import DefaultButton from '../components/DefaultButton';
 
 function TrilhaCard({ item, navigation }) {
   return (
     <View>
-      <Pressable 
+      <Pressable
         style={styles.card}
         onPress={() => navigation.navigate('Iniciar', { item })}
-      > 
-        <Text>
-          {item.title}
-        </Text>
+      >
+        <View style={styles.cardHeader}>
+          <Image
+            style={styles.roundImage}
+            source={require('../assets/flamboyant-laranja-esalq.jpg')}
+          />
+          <View style={styles.subCard}>
+            <Text style={styles.cardTitle}>
+              {item.title}
+            </Text>
+            <Text style={styles.cardSubtitle}>
+              7 Árvores 1.8 Km
+            </Text>
+          </View>
+        </View>
       </Pressable>
     </View>
   );
